6336294c8a0326a0167430437b531559647392a1
gabe <gabrielbruck1@gmail.com>
2020-04-26 16:21:36 +0300

    Excercise1

--- src/App.js
+++ src/App.js
@@ -7,48 +7,23 @@ import Box1 from "./Day1/box";
 
 function App() {
   return (
-    <div>
-      <Travel
-        name={"Munich"}
-        imageUrl={
-          "https://www.muenchen.de/media/shutterstock-2017/sehenswuerdigkeiten-2/rathaus-sonne-hp.jpg"
-        }
-        location={
-          "https://www.google.com/maps?q=munchen&rlz=1C5CHFA_enIL829IL829&um=1&ie=UTF-8&sa=X&ved=2ahUKEwisg_ek-4XpAhWFQEEAHfT6A_AQ_AUoAnoECDEQBA"
-        }
-      ></Travel>
-      <Travel
-        name={"Istanbul"}
-        imageUrl={"https://wallpapercave.com/wp/wp1813723.jpg"}
-        location={
-          "https://https://www.google.com/maps?q=istanbul+wallpaper&rlz=1C5CHFA_enIL829IL829&hl=en&um=1&ie=UTF-8&sa=X&ved=2ahUKEwijz4Cq_4XpAhVSoVwKHc2ZAOwQ_AUoA3oECCMQBQ.google.com/maps?q=munchen&rlz=1C5CHFA_enIL829IL829&um=1&ie=UTF-8&sa=X&ved=2ahUKEwisg_ek-4XpAhWFQEEAHfT6A_AQ_AUoAnoECDEQBA"
-        }
-      ></Travel>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Itc react app</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Me
+        </a>
+      </header>
     </div>
   );
 }
-
 export default App;
-
-// Excercise 1
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Itc react app</p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Click Me
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 // Excercise 2
 // <div>
